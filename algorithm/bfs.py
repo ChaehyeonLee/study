3672eb38129ebc6fd0d6d51b56ea4f48ae113676
Chaehyeon Lee <chyun412@naver.com>
2018-07-06 16:30:14 +0900

    small changes on annotation

--- algorithm/bfs.py
+++ algorithm/bfs.py
@@ -2,7 +2,7 @@
 #tree 전체를 탐색하려면 goal에 None을 넣고, target node가 있는 경우는 그 값을 goal에 넣으면 된다.
 def bfs(tree, root, target):
     visited = [] #방문한 node들이 순서대로 visited리스트에 들어간다. bfs한 결과를 나타냄
-    queue = [root] #방문할 node들이 queue에 저장된다.
+    queue = [root] ##방문할 node들이 queue에 저장됨. 먼저 root node를 queue에 넣음
     
     while queue:
         node = queue.pop(0)
@@ -25,5 +25,5 @@ test_tree = {'A': ['B', 'C'], 'B':['A', 'D'], 'C':['A', 'E', 'F'], 'D': ['B', 'G
 # bfs(tree, root, target) method 테스트
 print('# BFS Tests #')
 print('test1_output:', bfs(test_tree, 'A', None)) #'A'를 root node로 test_tree 전체를 bfs
-print('test2_output:', bfs(test_tree, 'B', 'C')) #'B'를 root node로 test_tree에서 'C'를 찾을 때 까지 bfs 
+print('test2_output:', bfs(test_tree, 'B', 'C')) #'B'를 root node로 'C'를 찾을 때 까지 bfs 
 print('test3_output:', bfs(test_tree, 'C', 'C')) #root node와 target node가 같을 때도 잘 동작함
