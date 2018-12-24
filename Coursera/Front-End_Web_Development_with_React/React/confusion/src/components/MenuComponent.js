f6d493cb47c0d0039b1428f4f854dfacf0451714
Chaehyeon Lee <chyun412@naver.com>
2018-12-24 12:47:55 +0900

    Test Mounting Lifecycle methods

--- Coursera/Front-End_Web_Development_with_React/React/confusion/src/components/MenuComponent.js
+++ Coursera/Front-End_Web_Development_with_React/React/confusion/src/components/MenuComponent.js
@@ -8,6 +8,11 @@ class Menu extends Component {
         this.state = {
             selectedDish: null
         }
+        console.log('Menu Component constructor is invoked');
+    }
+
+    componentDidMount() {
+        console.log('Menu Component componentDidMount is invoked');
     }
 
     onDishSelect(dish) {
@@ -48,6 +53,8 @@ class Menu extends Component {
                 </div>
             );
         });
+
+        console.log('Menu Component render is invoked');
 
         return (
             <div className="container">
