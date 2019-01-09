d16c3c23f3c9efbcd6e7c60d37ed8224ef6af607
Chaehyeon Lee <chyun412@naver.com>
2019-01-09 19:43:04 +0900

    move folder

--- Coursera/Recommender_Systems/1_Non-Personalized_and_Content-Based/assignment/nonpers-assignment/etc/lift-assoc.groovy
+++
@@ -1,7 +0,0 @@
-import org.lenskit.api.ItemBasedItemRecommender
-import org.lenskit.mooc.nonpers.assoc.LiftAssociationModelProvider
-import org.lenskit.mooc.nonpers.assoc.AssociationItemBasedItemRecommender
-import org.lenskit.mooc.nonpers.assoc.AssociationModel
-
-bind ItemBasedItemRecommender to AssociationItemBasedItemRecommender
-bind AssociationModel toProvider LiftAssociationModelProvider
--- Coursera/Recommender_Systems/1_Non-Personalized_and_Content-Based/assignment/nonpers-assignment/etc/mean.groovy
+++
@@ -1,4 +0,0 @@
-import org.lenskit.mooc.nonpers.mean.MeanItemBasedItemRecommender
-import org.lenskit.api.ItemBasedItemRecommender
-
-bind ItemBasedItemRecommender to MeanItemBasedItemRecommender
